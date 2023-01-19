3647a822fd255e197421b39fe895c72aecf0853a
ajinkya <ajeenky@gmail.com>
2023-01-19 17:12:06 +0530

    changed test class

--- src/com/vel/Demo2.java
+++ src/com/vel/Demo2.java
@@ -1,10 +1,12 @@
 package com.vel;
 
+import java.awt.List;
+
 public class Demo2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello all");
+		List list = new List();
 
 	}
 
